9379b3cc64baf1cfdec85d104939a929c38b59b7
ChrisWMichel <christopherw.michel@gmail.com>
2024-03-18 16:02:42 -0400

    Created .env file

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUmCxWEh6delwT80b-8Y23Hyk9aSObexo",
-  authDomain: "music-16d2a.firebaseapp.com",
-  projectId: "music-16d2a",
-  storageBucket: "music-16d2a.appspot.com",
-  messagingSenderId: "134892352117",
-  appId: "1:134892352117:web:c52f0728a8185bc88a3e3c"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_authDomain,
+  projectId: FIREBASE_projectId,
+  storageBucket: FIREBASE_storageBucket,
+  messagingSenderId:FIREBASE_messagingSenderId,
+  appId:FIREBASE_appId
 };
 
 
